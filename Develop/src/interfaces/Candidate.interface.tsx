d9fb145d339221ed9ba4080479abcbdf4dadec14
Cinnlight <cinnlight@gmail.com>
2024-11-06 15:27:41 -0700

    Adds TypeScript interface for the shape of the User object

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+// types.ts or within the component file
+export interface GithubUser {
+    login: string;
+    id: number;
+    avatar_url: string;
+    company?: string;
+    bio?: string;
+    html_url: string;
+  }
+  
