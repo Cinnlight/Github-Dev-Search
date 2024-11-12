0dbdf0cd2ef3bfaec6422c71397f1aa1bdb49bb3
Cinnlight <cinnlight@gmail.com>
2024-11-12 10:21:55 -0700

    Adds filter to include candidates with a bio or company listed. Changes to bootstrap dark theme for visibility.

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -2,37 +2,36 @@ import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Nav = () => {
-  return (
-
-    <nav className="navbar navbar-expand-lg navbar-custom">
-      <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Dev Search</Link>
-
-          <button 
-              className="navbar-toggler" 
-              type="button" 
-              data-bs-toggle="collapse" 
-              data-bs-target="#navbarNav" 
-              aria-controls="navbarNav" 
-              aria-expanded="false" 
-              aria-label="Toggle navigation"
-          >
-              <span className="navbar-toggler-icon"></span>
-          </button>
-          
-          <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                      <Link className="nav-link" to="/">Search</Link>
-                  </li>
-                  <li className="nav-item">
-                      <Link className="nav-link" to="/SavedCandidates">Saved Candidates</Link>
-                  </li>
-              </ul>
-          </div>
-      </div>
-    </nav>
-  )
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"> {/* Use navbar-dark and bg-dark for dark background */}
+            <div className="container-fluid">
+                <Link className="navbar-brand text-white" to="/">Dev Search</Link>
+        
+                <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <Link className="nav-link text-white" to="/">Search</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link text-white" to="/SavedCandidates">Saved Candidates</Link>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
